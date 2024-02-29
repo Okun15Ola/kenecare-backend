bbe253151d6bba2adfd2fb4c20ac3a954cd459fd
rolandeke <roladneke49@gmail.com>
2024-02-29 16:30:01 +0000

    updated ratelimiter to be used in production

--- src/utils/rate-limit.utils.js
+++ src/utils/rate-limit.utils.js
@@ -1,12 +1,14 @@
 const rateLimiter = require("express-rate-limit");
-
+const { nodeEnv } = require("../config/default.config");
 const limiter = (rotuer) => {
-  const limit = rateLimiter({
-    windowMs: 10 * 60 * 1000, // 10 minutes
-    max: 5, // Limit each IP to 5 requests per windowMs
-    message: "Too many requests, please try again later.",
-    headers: true,
-  });
-  rotuer.use(limit);
+  if (nodeEnv === "production") {
+    const limit = rateLimiter({
+      windowMs: 10 * 60 * 1000, // 10 minutes
+      max: 5, // Limit each IP to 5 requests per windowMs
+      message: "Too many requests, please try again later.",
+      headers: true,
+    });
+    rotuer.use(limit);
+  }
 };
 module.exports = limiter;
