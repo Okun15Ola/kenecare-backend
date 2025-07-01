092f2a354dc50691d4ea9e66362487fbf5697ea2
Mevizcode <mevizdaven@gmail.com>
2025-07-01 16:16:01 +0000

    refactor: test setup.js

--- test/setup.js
+++ test/setup.js
@@ -1,5 +1,5 @@
 // Force Jest to use local mock for DB connection
-jest.mock("../__tests__/__mock__/db.connection.js");
+jest.mock("../__tests__/__mocks__/db.connection.js");
 
 // Mock express-mysql-session to prevent real DB session creation
 jest.mock("express-mysql-session", () => {
