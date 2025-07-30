e24b2a28799df6fbcd9666b28f19c261384b9202
Mevizcode <mevizdaven@gmail.com>
2025-07-30 15:46:30 +0000

    refactor: update doctors queries add council registration checks

--- src/repository/queries/doctors.queries.js
+++ src/repository/queries/doctors.queries.js
@@ -1,83 +1,116 @@
+const DOCTOR_COLUMNS = `
+  d.doctor_id, d.title, d.first_name, d.middle_name, d.last_name, 
+  d.gender, d.professional_summary, d.profile_pic_url, 
+  d.specialization_id, ms.speciality_name, d.qualifications, 
+  d.consultation_fee, c.city_name, c.latitude, c.longitude, 
+  d.years_of_experience, d.is_profile_approved, d.user_id, 
+  u.mobile_number, u.email, u.user_type, u.is_account_active, u.is_online, 
+  dcr.registration_status
+`;
+
+const DOCTOR_JOINS = `
+  FROM doctors d
+  INNER JOIN users u ON d.user_id = u.user_id
+  INNER JOIN medical_specialities ms ON d.specialization_id = ms.speciality_id
+  INNER JOIN cities c ON d.city_id = c.city_id
+  INNER JOIN doctors_council_registration dcr ON d.doctor_id = dcr.doctor_id
+`;
+
+const APPROVED_DOCTOR_CONDITIONS = `
+  WHERE d.is_profile_approved = 1
+  AND dcr.registration_status = 'approved'
+  AND dcr.certificate_expiry_date >= CURDATE()
+`;
+
 module.exports = {
   GET_ALL_DOCTORS: `
-    SELECT doctor_id, title, first_name, middle_name, last_name, gender, professional_summary, profile_pic_url, doctors.specialization_id, speciality_name, qualifications, consultation_fee, city_name, latitude, longitude, years_of_experience, is_profile_approved, doctors.user_id, mobile_number, email, user_type, is_account_active, is_online
-    FROM doctors
-    INNER JOIN users ON doctors.user_id = users.user_id
-    INNER JOIN medical_specialities ON doctors.specialization_id = medical_specialities.speciality_id
-    INNER JOIN cities ON doctors.city_id = cities.city_id
-    WHERE is_profile_approved = 1
+    SELECT ${DOCTOR_COLUMNS}
+    ${DOCTOR_JOINS}
+    ${APPROVED_DOCTOR_CONDITIONS}
   `,
-  GET_DOCTORS_COUNT:
-    "SELECT COUNT(*) as totalRows FROM doctors WHERE is_profile_approved = 1;",
+  GET_DOCTORS_COUNT: `
+    SELECT COUNT(*) as totalRows
+    ${DOCTOR_JOINS}
+    ${APPROVED_DOCTOR_CONDITIONS}
+  `,
   SEARCH_DOCTOR_BY_QUERY: `
-  SELECT doctor_id, title, first_name, middle_name, last_name, gender, professional_summary, profile_pic_url,
-         doctors.specialization_id, speciality_name, qualifications, consultation_fee, city_name, latitude, longitude,
-         years_of_experience, is_profile_approved, doctors.user_id, mobile_number, email, user_type, is_account_active, is_online
-  FROM doctors
-  INNER JOIN users ON doctors.user_id = users.user_id
-  INNER JOIN medical_specialities ON doctors.specialization_id = medical_specialities.speciality_id
-  INNER JOIN cities ON doctors.city_id = cities.city_id
-  WHERE doctors.city_id = ?
+    SELECT ${DOCTOR_COLUMNS}
+    ${DOCTOR_JOINS}
+    WHERE d.city_id = ?
     AND (
-      doctors.first_name LIKE ?
-      OR doctors.middle_name LIKE ?
-      OR doctors.last_name LIKE ?
-      OR doctors.specialization_id LIKE ?
-      OR speciality_name LIKE ?
-    ) AND is_profile_approved = 1
-`,
+      d.first_name LIKE ?
+      OR d.middle_name LIKE ?
+      OR d.last_name LIKE ?
+      OR d.specialization_id LIKE ?
+      OR ms.speciality_name LIKE ?
+    )
+    AND d.is_profile_approved = 1
+    AND dcr.registration_status = 'approved'
+    AND dcr.certificate_expiry_date >= CURDATE()
+  `,
   COUNT_SEARCH_DOCTOR_BY_QUERY: `
   SELECT COUNT(*) AS totalRows
-  FROM doctors
-  INNER JOIN users ON doctors.user_id = users.user_id
-  INNER JOIN medical_specialities ON doctors.specialization_id = medical_specialities.speciality_id
-  INNER JOIN cities ON doctors.city_id = cities.city_id
-  WHERE doctors.city_id = ?
+  ${DOCTOR_JOINS}
+  WHERE d.city_id = ?
   AND (
-    doctors.first_name LIKE ?
-    OR doctors.middle_name LIKE ?
-    OR doctors.last_name LIKE ?
-    OR doctors.specialization_id LIKE ?
-    OR speciality_name LIKE ?
+    d.first_name LIKE ?
+    OR d.middle_name LIKE ?
+    OR d.last_name LIKE ?
+    OR d.specialization_id LIKE ?
+    OR ms.speciality_name LIKE ?
   )
-  AND is_profile_approved = 1;
+  AND d.is_profile_approved = 1
+  AND dcr.registration_status = 'approved'
+  AND dcr.certificate_expiry_date >= CURDATE()
   `,
   GET_DOCTOR_BY_ID: `
-    SELECT doctor_id, title, first_name, middle_name, last_name, gender, professional_summary, profile_pic_url, doctors.specialization_id, speciality_name, qualifications, consultation_fee, city_name, years_of_experience, is_profile_approved, doctors.user_id, mobile_number, email, user_type, is_account_active, is_online
-    FROM doctors
-    INNER JOIN users ON doctors.user_id = users.user_id
-    INNER JOIN medical_specialities ON doctors.specialization_id = medical_specialities.speciality_id
-    INNER JOIN cities ON doctors.city_id = cities.city_id
-    WHERE doctor_id = ? AND is_profile_approved = 1 LIMIT 1;
+    SELECT ${DOCTOR_COLUMNS}
+    ${DOCTOR_JOINS}
+    WHERE d.doctor_id = ?
+    AND d.is_profile_approved = 1
+    AND dcr.registration_status = 'approved'
+    AND dcr.certificate_expiry_date >= CURDATE()
+    LIMIT 1
   `,
   GET_DOCTOR_BY_USER_ID: `
-    SELECT doctor_id, title, first_name, middle_name, last_name, gender, professional_summary, profile_pic_url, doctors.specialization_id, speciality_name, qualifications, consultation_fee, city_name, years_of_experience, is_profile_approved, doctors.user_id, notification_token, mobile_number, email, user_type, is_account_active, is_online
-    FROM doctors
-    INNER JOIN users ON doctors.user_id = users.user_id
-    INNER JOIN medical_specialities ON doctors.specialization_id = medical_specialities.speciality_id
-    INNER JOIN cities ON doctors.city_id = cities.city_id
-    WHERE doctors.user_id = ? LIMIT 1;
+    SELECT ${DOCTOR_COLUMNS}, u.notification_token
+    ${DOCTOR_JOINS}
+    WHERE d.user_id = ?
+    LIMIT 1
   `,
-  GET_DOCTOR_BY_CITY_ID:
-    "SELECT * FROM doctors WHERE city_id = ? AND is_profile_approved = 1",
+  GET_DOCTORS_BY_CITY_ID: `
+    SELECT ${DOCTOR_COLUMNS}
+    ${DOCTOR_JOINS}
+    WHERE d.city_id = ?
+    AND d.is_profile_approved = 1
+    AND dcr.registration_status = 'approved'
+    AND dcr.certificate_expiry_date >= CURDATE()
+  `,
   GET_DOCTORS_COUNT_BY_CITY:
-    "SELECT COUNT(*) AS totalRows FROM doctors WHERE city_id = ? AND is_profile_approved = 1;",
-  GET_DOCTOR_BY_SPECIALIZATION_ID: `
-    SELECT doctor_id, title, first_name, middle_name, last_name, gender, professional_summary, profile_pic_url, doctors.specialization_id, speciality_name, qualifications, consultation_fee, city_name, years_of_experience, is_profile_approved, doctors.user_id, mobile_number, email, user_type, is_account_active, is_online
-    FROM doctors
-    INNER JOIN users ON doctors.user_id = users.user_id
-    INNER JOIN medical_specialities ON doctors.specialization_id = medical_specialities.speciality_id
-    INNER JOIN cities ON doctors.city_id = cities.city_id
-    WHERE doctors.specialization_id = ? AND is_profile_approved = 1
+    "SELECT COUNT(*) AS totalRows FROM doctors INNER JOIN doctors_council_registration dcr ON doctors.doctor_id = dcr.doctor_id WHERE city_id = ? AND is_profile_approved = 1 AND dcr.registration_status = 'approved' AND dcr.certificate_expiry_date >= CURDATE();",
+  GET_DOCTORS_BY_SPECIALIZATION_ID: `
+    SELECT ${DOCTOR_COLUMNS}
+    ${DOCTOR_JOINS}
+    WHERE d.specialization_id = ?
+    AND d.is_profile_approved = 1
+    AND dcr.registration_status = 'approved'
+    AND dcr.certificate_expiry_date >= CURDATE()
   `,
-  GET_DOCTOR_BY_SPECIALIZATION_ID_COUNT: `
+  GET_DOCTORS_BY_SPECIALIZATION_ID_COUNT: `
     SELECT COUNT(*) AS totalRows
     FROM doctors
-    WHERE specialization_id = ? AND is_profile_approved = 1;
+    INNER JOIN doctors_council_registration dcr ON doctors.doctor_id = dcr.doctor_id
+    WHERE specialization_id = ? AND is_profile_approved = 1 AND dcr.registration_status = 'approved' AND dcr.certificate_expiry_date >= CURDATE();
   `,
-  GET_DOCTOR_BY_HOSPITAL_ID: "SELECT * FROM doctors WHERE hospital_id = ?",
-  GET_DOCTOR_BY_HOSPITAL_ID_COUNT:
-    "SELECT COUNT(*) AS totalRows FROM doctors WHERE hospital_id = ?;",
+  GET_DOCTORS_BY_HOSPITAL_ID: `
+  SELECT ${DOCTOR_COLUMNS}
+    ${DOCTOR_JOINS}
+  WHERE hospital_id = ? AND is_profile_approved = 1
+  AND dcr.registration_status = 'approved' 
+  AND dcr.certificate_expiry_date >= CURDATE()
+  `,
+  GET_DOCTORS_BY_HOSPITAL_ID_COUNT:
+    "SELECT COUNT(*) AS totalRows FROM INNER JOIN doctors_council_registration dcr ON doctors.doctor_id = dcr.doctor_id doctors WHERE hospital_id = ? AND dcr.registration_status = 'approved' AND dcr.certificate_expiry_date >= CURDATE();",
   GET_DOCTOR_COUNCIL_REGISTRATION_BY_DOCTOR_ID: `
     SELECT council_registration_id, dcr.doctor_id, first_name, last_name, gender, doctors.specialization_id, speciality_name, profile_pic_url, council_name, registration_number, registration_year, registration_document_url, certificate_issued_date, certificate_expiry_date, registration_status, rejection_reason, fullname as 'verified_by'
     FROM doctors_council_registration as dcr
@@ -136,10 +169,4 @@ module.exports = {
   `,
   UPDATE_DOCTOR_PROFILE_PICTURE:
     "UPDATE doctors SET profile_pic_url = ? WHERE doctor_id = ? AND is_profile_approved = 1",
-  APPROVE_DOCTOR_PROFILE:
-    "UPDATE doctors SET is_profile_approved = 1, approved_by = ? WHERE doctor_id = ?",
-  APPROVE_DOCTOR_COUNCIL_REGISTRATION:
-    "UPDATE doctors_council_registration SET registration_status = 'approved', verified_by = ? WHERE council_registration_id = ?",
-  REJECT_DOCTOR_COUNCIL_REGISTRATION:
-    "UPDATE doctors_council_registration SET registration_status = 'rejected', rejection_reason= ?, verified_by = ? WHERE council_registration_id = ?",
 };
