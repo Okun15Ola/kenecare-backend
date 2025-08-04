7d37fabd444a862676e0671ad21566126a8de6d2
rolandeke <roladneke49@gmail.com>
2025-08-04 09:41:54 +0000

    fix: fixed doctor profile approval error in admin services

--- src/repository/admin-doctors.repository.js
+++ src/repository/admin-doctors.repository.js
@@ -182,7 +182,7 @@ exports.updateDoctorProfilePictureById = async ({ doctorId, imageUrl }) => {
 };
 
 exports.approveDoctorProfileByDoctorId = async ({ doctorId, approvedBy }) => {
-  return query(queries.APPROVE_PROFILE, [approvedBy, doctorId]);
+  return query(queries.APPROVE_DOCTOR_PROFILE, [approvedBy, doctorId]);
 };
 
 exports.approveDoctorMedicalCouncilRegistrationById = async ({
