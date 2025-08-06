d857c00dea36bcdc65c04b724fa2c87a8014544e
Mevizcode <mevizdaven@gmail.com>
2025-08-06 17:00:33 +0000

    refactor: update admin blog category validation

--- src/validations/admin/blog-category.validations.js
+++ src/validations/admin/blog-category.validations.js
@@ -4,9 +4,11 @@ exports.blogCategoryValidations = [
   body("name")
     .notEmpty()
     .withMessage("Blog Category Name is required")
+    .bail()
     .toLowerCase()
     .trim()
     .isLength({ max: 150, min: 3 })
     .withMessage("Must be more than 3 characters long")
+    .bail()
     .escape(),
 ];
