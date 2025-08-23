d13e5be6d77f5fcc7f95c676bc49b252bfced8b7
Mevizcode <mevizdaven@gmail.com>
2025-08-23 13:25:42 +0000

    refactor: update follow up route

--- src/routes/api/patients/follow-up.routes.js
+++ src/routes/api/patients/follow-up.routes.js
@@ -2,14 +2,28 @@ const router = require("express").Router();
 const {
   GetPatientFollowUpMetricsController,
 } = require("../../../controllers/patients/appointments.controller");
+const {
+  getPatientAppointmentFollowUpController,
+} = require("../../../controllers/patients/follow-up.controller");
+const {
+  AppointmentIdValidation,
+} = require("../../../validations/appointments.validations");
+const { Validate } = require("../../../validations/validate");
 const { limiter } = require("../../../utils/rate-limit.utils");
 const {
   authenticateUser,
   authorizePatient,
 } = require("../../../middlewares/auth.middleware");
 
-router.use(authenticateUser, limiter, authorizePatient); // Authentication middleware & Rate limiting middleware applied to all routes in this router
+router.use(authenticateUser, limiter, authorizePatient);
 
 router.get("/metrics", GetPatientFollowUpMetricsController);
+
+router.get(
+  "/appointments/:id",
+  AppointmentIdValidation,
+  Validate,
+  getPatientAppointmentFollowUpController,
+);
 
 module.exports = router;
